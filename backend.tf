aa5e1ef7f45a1583f17a60c7f652b3e879973770
Ganesh-taur <134471413+Ganesh-taur@users.noreply.github.com>
2025-01-04 16:07:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "your-unique-terraform-state-bucket"
+    bucket = "your-unique-terraform-state-bucket-mk"
     key    = "terraform/statefile.tfstate"
     region = "us-east-1"  # Change to your region
   }
